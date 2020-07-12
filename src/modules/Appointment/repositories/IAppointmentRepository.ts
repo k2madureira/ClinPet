fefd9c8fe62ee690c9af605bdbd07ca5f668d9bf
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-07-12 18:43:45 -0300

    Appointment Repository

--- src/modules/Appointment/repositories/IAppointmentRepository.ts
+++ src/modules/Appointment/repositories/IAppointmentRepository.ts
@@ -1,6 +1,9 @@
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 
 export default interface IAppointmentRepository {
-  create(data: ICreateAppointmentDTO): Promise<ICreateAppointmentDTO>;
+  list(): Promise<ICreateAppointmentDTO[]>;
   listById(id: string): Promise<ICreateAppointmentDTO[]>;
+  create(data: ICreateAppointmentDTO): Promise<ICreateAppointmentDTO>;
+  update(id: string): Promise<ICreateAppointmentDTO>;
+  delete(id: string): Promise<void>;
 }
